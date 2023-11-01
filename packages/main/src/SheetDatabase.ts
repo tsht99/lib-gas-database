3c96ff05b8c859ceeedf3c00f5d7108838cbeb88
tsht99 <51362043+tsht99@users.noreply.github.com>
2023-11-01 11:45:12 +0900

    リファクタ

--- packages/main/src/SheetDatabase.ts
+++ packages/main/src/SheetDatabase.ts
@@ -1,31 +1,31 @@
 import { setValuesToSheetRange } from '@tsht99/lib-gas-utils'
 
 /**
- * スプレッドシートをデータベースとして操作するためのクラス。
+ * シートで作成したテーブル
  */
-export class SheetDatabase {
+export class Table {
     /**
-     * データベースの名前。
+     * テーブル名
      */
     public readonly name
 
     /**
-     * データベースと関連付けられたスプレッドシートのシート。
+     * テーブルの元となるシート
      */
     private readonly sheet
 
     /**
-     * スプレッドシートが見つからない場合のエラーメッセージ。
+     * テーブルが見つからない場合のエラーメッセージ
      */
-    private readonly SHEET_NOT_FOUND = 'sheet not found'
+    private readonly TABLE_NOT_FOUND = 'Table not found'
 
     /**
-     * レコードが見つからない場合のエラーメッセージ。
+     * レコードが見つからない場合のエラーメッセージ
      */
-    private readonly RECORD_NOT_FOUND = 'record not found'
+    private readonly RECORD_NOT_FOUND = 'Record not found'
 
     /**
-     * データベース内のレコードの数。
+     * レコードの数
      */
     get length() {
         // 何も入力されていないときは 0
@@ -33,13 +33,12 @@ export class SheetDatabase {
     }
 
     /**
-     * Database クラスの新しいインスタンスを作成します。
-     * @param sheetName - 関連付けるスプレッドシートのシート名。
-     * @throws スプレッドシートが見つからない場合にスローされるエラー。
+     * @param name - テーブル名
+     * @throws テーブルが見つからない
      */
-    constructor(sheetName: string) {
-        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName)
-        if (!sheet) throw new Error(this.SHEET_NOT_FOUND)
+    constructor(name: string) {
+        const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(name)
+        if (!sheet) throw new Error(this.TABLE_NOT_FOUND)
 
         // 1 行目しか使わない
         // 見た目は悪いけど処理速度は上がる
@@ -55,21 +54,21 @@ export class SheetDatabase {
             protect.setWarningOnly(true)
         }
 
-        this.name = sheetName
+        this.name = name
         this.sheet = sheet
     }
 
     /**
-     * 新しいレコードをデータベースに追加します。
-     * @param records - 追加するレコードの配列。
+     * レコードを追加
+     * @param records - 追加するレコード
      */
     public create(records: unknown[]) {
         setValuesToSheetRange(this.sheet, [records], 1, this.length + 1)
     }
 
     /**
-     * データベースの全てのデータを読み取ります。
-     * @returns 全てのデータ。
+     * レコードを取得
+     * @returns 全てのレコード
      */
     public read() {
         if (!this.length) return []
@@ -77,10 +76,10 @@ export class SheetDatabase {
     }
 
     /**
-     * 指定されたインデックス位置に新しいデータを設定します。
-     * @param record - 設定する新しいデータ。
-     * @param index - データを設定するインデックス。
-     * @throws 指定されたインデックス位置が存在しない場合にスローされるエラー。
+     * レコードを更新
+     * @param record - 更新するレコード
+     * @param index - 更新するインデックス
+     * @throws レコードが見つからない
      */
     public update(record: unknown, index: number) {
         if (this.length < index) throw new Error(this.RECORD_NOT_FOUND)
@@ -88,9 +87,9 @@ export class SheetDatabase {
     }
 
     /**
-     * 指定されたインデックス位置を削除します。
-     * @param index - 削除するインデックス。
-     * @throws 指定されたインデックス位置が存在しない場合にスローされるエラー。
+     * レコードを削除
+     * @param index - 削除するインデックス
+     * @throws レコードが見つからない
      */
     public delete(index: number) {
         if (this.length < index) throw new Error(this.RECORD_NOT_FOUND)
