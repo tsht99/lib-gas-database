3e6e5fe2b24581d82300325ad5427dcc7eef220f
tsht99 <51362043+tsht99@users.noreply.github.com>
2023-11-08 20:57:16 +0900

    実装準備

--- packages/test/scripts/push.ts
+++ packages/test/scripts/push.ts
@@ -1,11 +1,22 @@
-import * as fs from 'fs'
 import { execSync } from 'child_process'
+import { config } from 'dotenv'
+import * as fs from 'fs'
 
-const sourcePath = './appsscript.json'
-const destPath = './dist/appsscript.json'
+config()
 
-fs.copyFile(sourcePath, destPath, (err) => {
-    if (err) throw err
-    const buffer = execSync('clasp push')
-    console.log(buffer.toString())
-})
+fs.copyFileSync('./appsscript.json', './dist/appsscript.json')
+
+// Google Apps Script で環境変数が使えるようにする
+const adminApiKey = process.env.ADMIN_API_KEY
+const userApiKeys = process.env.USER_API_KEYS
+const text = `
+const process = {
+    env: {
+        ADMIN_API_KEY: "${adminApiKey}",
+        USER_API_KEYS: \`${userApiKeys}\`
+    }
+}`
+fs.writeFileSync('./dist/.env.js', text)
+
+const buffer = execSync('clasp push')
+console.log(buffer.toString())
